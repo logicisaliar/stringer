4ea9ab362c53cfb8f079a6a956c6abead0f96620
Jash Shah <jash.shah@live.com>
2018-09-14 16:48:10 +0530

    Finish all one-time substitutions

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,7 +1,64 @@
 class PhrasesController < ApplicationController
   def new
+    @phrase = Phrase.new
+  end
+
+  def create
+    @phrase = Phrase.new(phrase_params)
+    old_phrase = @phrase.old_string
+    array = grammify(old_phrase)
+    @phrase.new_string = array[-1]
+    @phrase.old_string = array[0]
+    if @phrase.save
+      redirect_to phrase_path(@phrase)
+    else
+      render new
+    end
   end
 
   def show
+    set_phrase
   end
+
+  private
+
+  def phrase_params
+    params.require(:phrase).permit(:old_string)
+  end
+
+  def set_phrase
+    @phrase = Phrase.find(params[:id])
+  end
+
+  def grammify(phrase)
+    phrase += " "
+    process = []
+    process << phrase
+    phrase.upcase!
+    phrase.gsub!(',', ' ,')
+    phrase.gsub!(/\WNR\W/,'Near ')
+    phrase.gsub!(/\WOP\W/,'Opposite ')
+    phrase.gsub!(/\WOPP\W/,'Opposite ')
+    phrase.gsub!(/\WTE\W/,'Tehsil ')
+    phrase.gsub!(/\WBH\W/,'Behind ')
+    phrase.gsub!(/\WRD/,'Road ')
+    phrase.gsub!('BYE PASS', 'Bypass')
+    phrase.gsub!('BYEPASS', 'Bypass')
+    phrase.gsub!('(', ' (')
+    phrase.gsub!(')', ') ')
+    phrase.gsub!(';', ':')
+    phrase.gsub!('MIDC', 'M. I. D. C.')
+    phrase.gsub!('GIDC', 'G. I. D. C.')
+
+    while(phrase =~ /  /)
+      phrase.gsub!('  ', ' ')
+    end
+
+    # At the end
+    phrase.gsub!(' ,', ',')
+    phrase.gsub!(',', ', ')
+    process << phrase.titlecase
+    return process
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'phrases/new'
-  get 'phrases/show'
-  get 'new/show'
   root to: 'pages#home'
+  resources :phrases, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
